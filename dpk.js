9f14176c9acd6ca09fc73e536af4889d4d604125
david <david@cerah.co>
2023-03-05 03:04:21 +0800

    Check that candidate exists is redundant now that that part of the code isn't reached if event doesn't exist.

--- dpk.js
+++ dpk.js
@@ -15,12 +15,8 @@ exports.deterministicPartitionKey = (event) => {
     candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
