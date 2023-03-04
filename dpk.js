ab0d9091260ef39fc44950389c99a4043c75e056
david <david@cerah.co>
2023-03-05 03:02:29 +0800

    Function is easier to read if the program flow is interrupted with a return instead of branching.

--- dpk.js
+++ dpk.js
@@ -2,16 +2,17 @@ const crypto = require("crypto");
 
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
+  
+  if (!event) return TRIVIAL_PARTITION_KEY;
+  
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (event.partitionKey) {
+    candidate = event.partitionKey;
+  } else {
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
   if (candidate) {
