0f891b145827d5027fe5e13846263322cee16775
david <david@cerah.co>
2023-03-05 03:07:19 +0800

    The checks that the partition key is a string and its length is not greater than 256 only make sense the partition key is already present in the event. Otherwise, we know it's a string and its length, since we generate it ourselves.

--- dpk.js
+++ dpk.js
@@ -10,16 +10,16 @@ exports.deterministicPartitionKey = (event) => {
 
   if (event.partitionKey) {
     candidate = event.partitionKey;
+    if (typeof candidate !== "string") {
+      candidate = JSON.stringify(candidate);
+    }
+    if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+      candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    }
   } else {
     const data = JSON.stringify(event);
     candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
-  if (typeof candidate !== "string") {
-    candidate = JSON.stringify(candidate);
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
   return candidate;
 };
