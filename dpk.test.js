12096ecb8abdaf7cc9131e4609674d447f6fec0f
david <david@cerah.co>
2023-03-05 03:40:54 +0800

    Added missing test for the cases where the partition key exists but it's longer than 256 characters, and also for the edge case where the partition key exists and is a string but it's empty (good to cover that in case something in the existing code base relies on that, although it was probably never intentional, so that the code isn't broken accidentally if someone changes that behaviour later).

--- dpk.test.js
+++ dpk.test.js
@@ -12,16 +12,30 @@ describe("deterministicPartitionKey", () => {
     expect(key.length).toBe(128);
   });
   
-  it("the partition key returned is generated using sha3-512 on the JSON-stringified event if it doesn't contain a partitionKey field yet", () => {
+  it("returns a key generated using sha3-512 on the JSON-stringified event if it doesn't contain a partitionKey field yet", () => {
     const event = {someField: "somevalue"};
     const key = deterministicPartitionKey(event);
     expect(key).toBe(crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex"));
   });
   
-  it("returns the event partitionKey if there's one already present and it's a string", () => {
+  it("returns the event partitionKey if there's one already present and it's a string and the string isn't empty", () => {
     const existingKey = "somekey";
     const key = deterministicPartitionKey({partitionKey: existingKey});
     expect(key).toBe(existingKey);
+  });
+  
+  it("returns the hash of the JSON-stringified event even if there's a partitionKey present and is a string, if the partition key is empty", () => {
+    const existingKey = "";
+    const event = {partitionKey: existingKey};
+    const key = deterministicPartitionKey(event);
+    expect(key).toBe(crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex"));
+  });
+  
+  it("returns a hash of the existing partitionKey if there's one already present and is a string, but the partition key is longer than 256 chars", () => {
+    const veryLongExistingKey = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567";
+    const event = {partitionKey: veryLongExistingKey};
+    const key = deterministicPartitionKey(event);
+    expect(key).toBe(crypto.createHash("sha3-512").update(veryLongExistingKey).digest("hex"));
   });
   
   it("returns the JSON-stringified partitionKey of the event if there's one already present and it's not a string", () => {
@@ -29,4 +43,12 @@ describe("deterministicPartitionKey", () => {
     const key = deterministicPartitionKey({partitionKey: existingKey});
     expect(key).toBe(JSON.stringify(existingKey));
   });
+  
+  it("returns a hashed JSON-stringified partitionKey of the event if there's one already present and it's not a string and it's length is bigger than 256 characters after stringifying", () => {
+    const longNonStringExistingKey = {someField: "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"};
+    const event = {partitionKey: longNonStringExistingKey};
+    const key = deterministicPartitionKey(event);
+    expect(key).toBe(crypto.createHash("sha3-512").update(JSON.stringify(longNonStringExistingKey)).digest("hex"));
+  });
+  
 });
